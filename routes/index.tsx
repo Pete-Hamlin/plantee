4297306893f0e1d4191d9387f441e352f72f51fa
pete <work@pete-hamlin.com>
2024-06-18 11:12:24 +0100

    feat: Very basic login page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,16 +1,36 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
 import Header from "../components/Header.tsx";
+import type { Handlers, PageProps } from "$fresh/server.ts";
+import { getCookies } from "$std/http/cookie.ts";
+
+interface Data {
+  isAuthenticated: boolean;
+}
 
 const PLANTS = [
   "African Violet",
   "Alocasia",
   "Aloe",
-  "Amaryllis"
-]
+  "Amaryllis",
+];
 
-export default function Home() {
-  const count = useSignal(3);
+function Login() {
+  return (
+    <form method="post" action="/api/login">
+      <input type="text" name="username" />
+      <input type="password" name="password" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export const handler: Handlers = {
+  GET(req, ctx) {
+    const cookies = getCookies(req.headers);
+    return ctx.render!({ isAuthenticated: cookies.auth === "bar" });
+  },
+};
+
+export default function Home({ data }: PageProps<Data>) {
   return (
     <div>
       <Header />
@@ -24,17 +44,27 @@ export default function Home() {
             alt="the Fresh logo: a sliced lemon dripping with juice"
           />
           <h1 class="text-4xl font-bold">Welcome to Plantee</h1>
-          <p class="my-4">
-            Try adding a plant to get started.
+          <p>
+            You are currently {data.isAuthenticated ? "are" : "are not"}{" "}
+            logged in
           </p>
-          <Counter count={count} />
-          <ul>
-            {PLANTS.map((name) => <li key={name}>{name}</li>)}
-          </ul>
-        </div>
-        <div class="max-w-screen-md mx-auto flex flex-col justify-between relative">
-          <button>Add</button>
-          <button>Export</button>
+          {!data.isAuthenticated ? <Login /> : <a href="/api/logout">Logout</a>}
+          {data.isAuthenticated
+            ? (
+              <>
+                <p class="my-4">
+                  Try adding a plant to get started.
+                </p>
+                <ul>
+                  {PLANTS.map((name) => <li key={name}>{name}</li>)}
+                </ul>
+                <div class="max-w-screen-md mx-auto flex flex-col justify-between relative">
+                  <button>Add</button>
+                  <button>Export</button>
+                </div>
+              </>
+            )
+            : null}
         </div>
       </div>
     </div>
